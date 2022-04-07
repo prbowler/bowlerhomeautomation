49c6b8703cc30a517081a0fac4b70a4c2f244e3e
prbowler <prbowler@byui.edu>
2022-04-07 10:18:19 -0700

    fixes

--- snippets/logo.php
+++ snippets/logo.php
@@ -1,7 +1,7 @@
 
 <div id="logo">
     <a href="index.php">
-        <img class="logo" src="img/logo.jpg\" alt="Logo" height="50" width="50">
+        <img class="logo" src="/img/logo.jpg" alt="Logo" height="50" width="50">
     </a>
 </div>
 
